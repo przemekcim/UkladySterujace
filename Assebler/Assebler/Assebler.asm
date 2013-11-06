a68e0f42ff25cd31b058fc575dca60c83d45a41f
Przemysław Cimcioch <przemyslaw.cimcioch@uj.edu.pl>
2013-11-06 16:54:06 +0100

    Update Assebler.asm

--- Assebler/Assebler/Assebler.asm
+++ Assebler/Assebler/Assebler.asm
@@ -1,20 +1,28 @@
-.include "m32def.inc" 
+
+
+ .include "m32def.inc" 
 
 .macro delay_ms
-	PUSH R16
-	PUSH R24
-	PUSH R25
+	;PUSH R16
+	;PUSH R24
+	;PUSH R25
 
-	LDI R16, @0
+	LDI R16, @0                 ;1
+	; 3998 
+	LDI R17, 0x9E			;1
+	LDI R18, 0x0F			;1
+	; first loop shorter because of init
+	LDI R24, -2
+	LDI R25, 0xff
 
 	MAIN_LOOP:
 		; if (R16 == 0) break
 		CPI R16, 0x00			;1
 		BREQ END_MAIN_LOOP		;1 - no jump is common
 
-		; (R25, R24) = 3998
-		LDI R24, 0x9E			;1
-		LDI R25, 0x0F			;1
+		; add 3998, couse 4*3998 + 8 = 16'000 (1ms on 16kHz clock)
+		ADD R24, R17
+		ADC R25, R18
 
 		; 8 + 4*3998 = 16'000 - 1ms on 16kHz clock
 		MS_LOOP:
@@ -23,42 +31,21 @@
 
 		; decrement R16
 		DEC R16					;1
-		NOP						;1
-		RJMP MAIN_LOOP			;2
-	END_MAIN_LOOP:
+		NOP
+		NOP
+		RJMP MAIN_LOOP			;1
+	END_MAIN_LOOP:              ;1 (breq)
 
-	POP R25
-	POP R24
-	POP R16
+	;POP R25
+	;POP R24
+	;POP R16
 .endm
 
 
 
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;defs
-.def one=R24
-LDI one, 1
-.def full=R25
-LDI full, 0xFF
-.def zero=R26
-LDI zero, 0
-
-;inits
-LDI R16, 0x00
-LDI R17, 0x00
-LDI R18, 0x00
-
-
-;;;;;;;;;;;;;;;;
-OUT DDRA, full
-
 LOOP:
-	ADD R16, one
-	ADC R17, zero
-	ADC R18, zero
-	OUT PORTA, R18
-	;OUT PORTA, one
+	delay_ms 5
+	nop
 	RJMP LOOP
 	
