dfa4d535b6bdd611adc405bdc6463a2bb26be7fe
Przemysław Cimcioch <przemyslaw.cimcioch@uj.edu.pl>
2013-11-27 17:34:40 +0100

    Update Scheduler.c

--- CTaskMannager/Scheduler.c
+++ CTaskMannager/Scheduler.c
@@ -2,8 +2,10 @@
 
 Task scheduler_tasks[MAX_TASK];
 uint8_t scheduler_taskCount;
+uint8_t scheduler_displayCount;
+uint8_t* scheduler_display;
 
-void scheduler_init() 
+void scheduler_init(uint8_t* display)
 {
 	// send interrput on compare
 	TIMSK|=(1<<OCIE0);
@@ -14,13 +16,16 @@ void scheduler_init()
 	TCCR0|=(1<<WGM01);
 	
 	scheduler_taskCount = 0;
-	for(uint8_t i = 0; i<MAX_TASK; ++i) 
+	for(uint8_t i = 0; i<MAX_TASK; ++i)
 	{
 		scheduler_tasks[i].interval = NON_PERIODIC;
 		scheduler_tasks[i].ready = 0;
 		scheduler_tasks[i].task = 0;
 		scheduler_tasks[i].toGo = 0;
 	}
+	
+	scheduler_display = display;
+	scheduler_displayCount = 0;
 }
 
 void scheduler_run()
@@ -38,7 +43,7 @@ void scheduler_run()
 		} else {
 			++i;
 			if(i >= scheduler_taskCount)
-				i=0;					
+			i=0;
 		}
 	}
 }
@@ -64,7 +69,16 @@ void scheduler_addOneShotTask(TASK_PTR task, uint16_t interval)
 
 ISR(TIMER0_COMP_vect)
 {
-	for(uint8_t i = 0; i<scheduler_taskCount; ++i) 
+	++scheduler_displayCount;
+	if(scheduler_display && ((scheduler_displayCount&3)==0))
+	{
+		PORTB &= 0xf0;
+		PORTA = scheduler_display[(scheduler_displayCount>>2) & 0x3];
+		PORTB =~(1<<((scheduler_displayCount>>2) & 0x3));
+		;
+	}	
+	
+	for(uint8_t i = 0; i<scheduler_taskCount; ++i)
 	{
 		if(scheduler_tasks[i].interval != NON_PERIODIC)
 		{
@@ -75,7 +89,7 @@ ISR(TIMER0_COMP_vect)
 				scheduler_tasks[i].toGo = scheduler_tasks[i].interval;
 			}
 		} else {
-			if(scheduler_tasks[i].toGo > 0) 
+			if(scheduler_tasks[i].toGo > 0)
 			{
 				--(scheduler_tasks[i].toGo);
 				scheduler_tasks[i].ready = 1;
